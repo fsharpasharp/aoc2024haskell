2a93a231da9996e1292c053098642c925e02147c
fsharpasharp <jnliuu@gmail.com>
2025-09-10 23:27:27 +0200

    Implement day07

--- src/Day07.hs
+++ src/Day07.hs
@@ -2,35 +2,39 @@
 
 module Day07 where
 
+import Control.Monad
 import Solution
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import Control.Monad
 
 data Result = Result Integer [Integer] deriving (Show)
 
 file :: Parser [Result]
-file = some $ (do
-    res <- integer
-    _ <- chunk ": "
-    rest <- some (integer <* hspace)
-    return $ Result res rest) <* space
+file =
+  some $
+    ( do
+        res <- integer
+        _ <- chunk ": "
+        rest <- some (integer <* hspace)
+        return $ Result res rest
+    )
+      <* space
 
-
--- Optimization here to stop computing if we overshoot.
-values _ []     = []
-values ops (x:xs) = foldM step x xs
+-- We could stop computing if we overshoot since the operators only increase the accumulator.
+values _ [] = []
+values ops (x : xs) = foldM step x xs
   where
-    step acc y = [ op acc y | op <- ops ]
-
+    step acc y = [op acc y | op <- ops]
 
 solve ops = sum . fmap solve'
-    where solve' (Result ans comp) = if ans `elem` values ops comp then ans else 0
+  where
+    solve' (Result ans comp) = if ans `elem` values ops comp then ans else 0
 
 solution07 :: Solution [Result] Integer
-solution07 = Solution
-  { parseInput = parseOrDie $ file <* eof
-  , solvePart1 = solve [(+), (*)]
-  , solvePart2 = solve [(+), (*), \a b -> read (show a ++ show b)]
-  , files = ["data/Day07.in"]
-  }
+solution07 =
+  Solution
+    { parseInput = parseOrDie $ file <* eof,
+      solvePart1 = solve [(+), (*)],
+      solvePart2 = solve [(+), (*), \a b -> read (show a ++ show b)],
+      files = ["data/Day07.in"]
+    }
