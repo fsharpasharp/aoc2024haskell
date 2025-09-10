39c940a6b7aa874537575d71f07abd3e5cd4d2bf
fsharpasharp <jnliuu@gmail.com>
2025-09-10 23:21:42 +0200

    Implement day07

--- src/Day07.hs
+++ src/Day07.hs
@@ -5,23 +5,32 @@ module Day07 where
 import Solution
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import Control.Monad
 
 data Result = Result Integer [Integer] deriving (Show)
 
 file :: Parser [Result]
-file = some $ do
+file = some $ (do
     res <- integer
     _ <- chunk ": "
-    rest <- some (integer <* space)
-    return $ Result res rest
+    rest <- some (integer <* hspace)
+    return $ Result res rest) <* space
 
-s = debug "data/Day07example.in" (parseOrDie file) [id]
 
-solution07 = undefined
+-- Optimization here to stop computing if we overshoot.
+values _ []     = []
+values ops (x:xs) = foldM step x xs
+  where
+    step acc y = [ op acc y | op <- ops ]
+
+
+solve ops = sum . fmap solve'
+    where solve' (Result ans comp) = if ans `elem` values ops comp then ans else 0
+
 solution07 :: Solution [Result] Integer
 solution07 = Solution
-  { parseInput = file
-  , solvePart1 = undefined
-  , solvePart2 = undefined
-  , files = ["data/Day07example.in"]
+  { parseInput = parseOrDie $ file <* eof
+  , solvePart1 = solve [(+), (*)]
+  , solvePart2 = solve [(+), (*), \a b -> read (show a ++ show b)]
+  , files = ["data/Day07.in"]
   }
